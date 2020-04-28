c6fb18b4ab1cc84ee47e63729aac28470fb65071
lxbuaa2017 <1753312844@qq.com>
2020-04-28 20:49:52 +0800

    finished

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 首先要有python3的环境
 然后pip（或者conda） install scrapy
 
-Linux系统下直接进入项目根目录，运行run.sh脚本即可
+**Linux系统下直接进入项目根目录，运行run.sh脚本，简报即可生成在根目录**
 
-Windows系统下具体操作为：
-进入到trafficBriefing/trafficBriefing目录，
+**Windows系统下具体操作为：**
+进入到trafficBriefing/trafficBriefing目录，如果之前跑过爬虫目录下会有brief.json文件，有的话就先删掉
 命令行执行 scrapy crawl briefing -o brief.json -s FEED_EXPORT_ENCODING=UTF-8 
 将在trafficBriefing文件夹下生成brief.json
 再去运行trafficBriefing/trafficBriefing/img/util/picMod.py修改图片
--- briefGenerator/trainsition.py
+++ briefGenerator/trainsition.py
@@ -8,11 +8,44 @@ import re
 import jieba
 import time
 import os
+import docx
 from docx import Document
 from docx.shared import Pt
 from docx.shared import RGBColor
 from docx.shared import Inches
 from docx.oxml.ns import qn
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+
+def num_to_char(num):
+    """数字转中文"""
+    num=str(num)
+    new_str=""
+    num_dict={"0":u"零","1":u"一","2":u"二","3":u"三","4":u"四","5":u"五","6":u"六","7":u"七","8":u"八","9":u"九"}
+    listnum=list(num)
+    # print(listnum)
+    shu=[]
+    for i in listnum:
+        # print(num_dict[i])
+        shu.append(num_dict[i])
+    new_str="".join(shu)
+    # print(new_str)
+    return new_str
+
+def add_detail(influenced,document,seq):
+    sub_title=influenced[0]
+    run = document.add_paragraph().add_run(num_to_char(seq)+'、 '+sub_title)
+    run.font.name = u'黑体'
+    run._element.rPr.rFonts.set(qn('w:eastAsia'), u'黑体')
+    # 三号
+    run.font.size = Pt(16)
+    run.font.bold = True
+    snow_list = influenced[1:]
+    for i,each in enumerate(snow_list):
+        each = str(i+1)+'. '+each
+        run = document.add_paragraph().add_run(each)
+        run.font.name = u'仿宋_GB2312'
+        run._element.rPr.rFonts.set(qn('w:eastAsia'), u'仿宋_GB2312')
+        run.font.size = Pt(14)
 
 p=os.path.dirname(__file__)
 p = os.path.join(p,os.path.pardir)
@@ -21,8 +54,9 @@ with open('{}/trafficBriefing/trafficBriefing/brief.json'.format(p), 'r', encodi
     raw_list = json.load(fObj)
     raw = raw_list[-1]
 
-
-path='{}/brief.docx'.format(p)
+now = datetime.datetime.now()
+date = str(now.year) + '年' + str(now.month) + '月' + str(now.day) + '日'
+path='{}/'.format(p)+date+"公路.docx"
 if os.path.exists(path):  # 如果文件存在
     os.remove(path)  
 fd = open(path, mode="w", encoding="utf-8")
@@ -90,13 +124,51 @@ p1.add_break(docx.enum.text.WD_BREAK.PAGE)
 
 
 
-p2 = document.add_paragraph().add_run("附件")
-p2.font.name = u'方正小标宋简体'
-p2._element.rPr.rFonts.set(qn('w:eastAsia'), u'方正小标宋简体')
-# 小四对应12pt
-p2.font.size = Pt(14)
-title = "受天气影响的主要路段、海域、机场"
+p2_1 = document.add_paragraph().add_run("附件")
+p2_1.font.name = u'方正小标宋简体'
+p2_1._element.rPr.rFonts.set(qn('w:eastAsia'), u'方正小标宋简体')
+# 四号
+p2_1.font.size = Pt(14)
 
+title = "受天气影响的主要路段、海域、机场"
+temp = document.add_paragraph()
+temp.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+p2_2 = temp.add_run(title)
+p2_2.font.name = u'方正小标宋简体'
+p2_2._element.rPr.rFonts.set(qn('w:eastAsia'), u'方正小标宋简体')
+# 二号
+p2_2.font.size = Pt(22)
+p2_2.font.bold = True
+
+seq=1
+
+
+lenth = len(raw['snow_influenced'])
+if lenth>1:
+    add_detail(raw['snow_influenced'],document,seq)
+    seq+=1
+
+
+lenth = len(raw['fog_influenced'])
+if lenth>1:
+    add_detail(raw['fog_influenced'],document,seq)
+    seq+=1
+
+lenth = len(raw['rain_influenced'])
+if lenth>1:
+    add_detail(raw['rain_influenced'],document,seq)
+    seq+=1
+
+lenth = len(raw['thunder_influenced'])
+if lenth>1:
+    add_detail(raw['thunder_influenced'],document,seq)
+    seq+=1
+
+lenth = len(raw['other_influenced'])
+if lenth>0:
+    for each in raw['other_influenced']:
+        add_detail(each,document,seq)
+        seq+=1
 document.save(path)
 
 
